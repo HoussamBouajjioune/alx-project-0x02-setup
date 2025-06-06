1f4889f99d274ecb6eb915d1dd5d63ae4f3748d8
houssam bouajjioune <houssambouajjioune1998@gmail.com>
2025-06-06 23:51:12 +0100

    feat: add User interface and exampleUser constant

---
+++ alx-project-0x02/interfaces/index.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+}
+
+export const exampleUser: User = {
+    id: 1,
+    name: "Alice",
+    email: "alice@example.com"
+};
